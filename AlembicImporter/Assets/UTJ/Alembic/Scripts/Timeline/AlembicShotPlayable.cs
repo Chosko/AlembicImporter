1fba75c725c934906e5c8cd9884f5d8437dfaa97
Chosko <ruben.caliandro@gmail.com>
2018-01-19 10:18:02 +0100

    fixed a compiler error

--- AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicShotPlayable.cs
+++ AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicShotPlayable.cs
@@ -8,7 +8,7 @@ namespace UTJ.Alembic
     {
         public AlembicStreamPlayer streamPlayer { get; set; }
 
-        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        public override void ProcessFrame(Playable playable, UnityEngine.Playables.FrameData info, object playerData)
         {
             base.ProcessFrame(playable, info, playerData);
 
